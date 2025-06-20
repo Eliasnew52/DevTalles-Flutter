3341c477e634cd265c6b56a4a3d9f8bbee2abd42
Eliasnew52 <xxeliasnew52xx@gmail.com>
2025-06-20 16:00:43 -0600

    Added Google Fonts Dependency :rocket:

--- HelloWorldApp/hello_world_app/lib/src/screens/counter_functions_screen.dart
+++ HelloWorldApp/hello_world_app/lib/src/screens/counter_functions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CounterFunctionsScreen extends StatefulWidget {
 
@@ -20,7 +21,7 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
     return Scaffold(
         appBar: AppBar( //AppBar no es Constante debido a que sus dimensiones son dinamicas en dependencia de la pantalla
           backgroundColor: Colors.greenAccent,
-          title: const Center(child: Text('Counter Functions', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)),
+          title: Center(child: Text('Counter Functions', style: GoogleFonts.rajdhani(fontWeight: FontWeight.bold, color: Colors.black,))),
           actions: [
             IconButton(
             icon: Icon(Icons.refresh_rounded),
@@ -36,8 +37,8 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
             //Para tomar en Centro de la Columna, usamos el mainAxisAligment
             mainAxisAlignment: MainAxisAlignment.center,
              children:  [
-               Text(clickCounter == 1 ?'Click' : 'Clicks', style: const TextStyle( fontSize: 28, fontWeight: FontWeight.bold)),
-              Text('$clickCounter', style:  const TextStyle( fontSize: 160, fontWeight: FontWeight.w100, ),),
+              Text(clickCounter == 1 ?'Click' : 'Clicks', style: GoogleFonts.rajdhani(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.black,)),
+              Text('$clickCounter', style: GoogleFonts.rajdhani(fontSize: 100, fontWeight: FontWeight.bold, color: Colors.black,)),
             ],
           ),
         ),
@@ -46,15 +47,31 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             //Refresh
-            CustomButton(icon: Icons.refresh_rounded),
+            CustomButton(icon: Icons.refresh_rounded,
+            onPressed: (){
+              setState(() {
+                clickCounter = 0;
+              });
+            },),
             const SizedBox( height: 10),
           
             //Sum Button
-            CustomButton(icon: Icons.plus_one_rounded),
+            CustomButton(icon: Icons.plus_one_rounded,
+            onPressed: (){
+              setState(() {
+                clickCounter++;
+              });
+            },),
 
             const SizedBox( height: 10),
             //Minus Button
-            CustomButton(icon: Icons.exposure_minus_1_rounded),
+            CustomButton(icon: Icons.exposure_minus_1_rounded,
+            onPressed: () {
+              setState(() {
+                if(clickCounter == 0) return;
+                clickCounter--;
+              });
+            },),
           ],
 
         )
@@ -64,22 +81,22 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
 
 class CustomButton extends StatelessWidget {
 
+
   final IconData icon;
+  final VoidCallback? onPressed;
 
   const CustomButton({
     super.key,
     required this.icon,
-
+    this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      shape: const StadiumBorder(),
-      onPressed: ()  {
-            
-              },
-              child:   Icon(icon)
+      enableFeedback: true,
+      onPressed: onPressed,
+      child:   Icon(icon)
               );
   }
 }
