65059480645f8bb38769c3c970dee88dc5c47d52
Eliasnew52 <xxeliasnew52xx@gmail.com>
2025-06-26 07:57:14 -0600

    Started with the Cyberpunk UI

--- YesNoApp/yesnoapp/lib/presentation/chat/chat_screen.dart
+++ YesNoApp/yesnoapp/lib/presentation/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ChatScreen  extends StatelessWidget{
   const ChatScreen({super.key});
@@ -6,17 +7,70 @@ class ChatScreen  extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Padding(
-          padding:  EdgeInsets.all(4.0),
-          child: CircleAvatar(
-            backgroundImage: NetworkImage('https://www.hleatherjackets.com/wp-content/uploads/2019/07/46646446646.jpg'),
+      appBar: _ChatAppBar(),
+      body: _ChatView(),
+    );
+    
+  }
+}
+
+// ChatBar is the AppBar Widget for the chat screen
+// It contains a leading CircleAvatar and a title "Chat"
+// The CircleAvatar has a fixed image URL
+class _ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const _ChatAppBar();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: AppBar(
+        flexibleSpace: Container(
+          decoration:  BoxDecoration(
+            border: Border.all(color: Colors.amber, width: 3),
+            color: Color.fromARGB(255, 145, 34, 3),
           ),
         ),
-        title:  Text('Chat'),
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(backgroundImage: NetworkImage('https://www.hleatherjackets.com/wp-content/uploads/2019/07/46646446646.jpg'),),
+        ),
+        title: Text('CHAT',style: GoogleFonts.rajdhani(fontWeight: FontWeight.bold,color: Colors.amber,),),
         centerTitle: false,
       ),
     );
-    
+  }
+}
+//ChatView is the main content of the chat screen
+//It Returns the container widget for the chat view
+class _ChatView extends StatelessWidget {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                     Color.fromARGB(255, 145, 34, 3),
+                    Color.fromARGB(255, 0, 0, 0), 
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+          ),
+          Text('Mundo')
+        ],
+      ),
+    );
   }
 }
